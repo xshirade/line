1eae4f45ee2d5a9f845eed28847ff5562cd0d713
Satoshi Hirade <shirade222@gmail.com>
2018-11-12 23:33:12 +0900

    Added heroku codes

--- app.js
+++ app.js
@@ -1,16 +1,21 @@
-var express = require('express');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+const express = require('express');
+const cookieParser = require('cookie-parser');
+const logger = require('morgan');
+const line = require("@line/bot-sdk");
 
-var indexRouter = require('./routes/index');
-
-var app = express();
+const app = express();
 
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 
-app.use('/', indexRouter);
+app.use('/webhook', line.middleware({
+    channelAccessToken: process.env.LINE_ACCESS_TOKEN,
+    channelSecret: process.env.LINE_CHANNEL_SECRET 
+}), (req, res) => {
+    console.log(req.body);
+    res.sendStatus(200);
+});
 
 module.exports = app;
