0f40fba68d145580a68d815ea2868191afe68d61
Satoshi Hirade <shirade222@gmail.com>
2018-11-13 00:51:51 +0900

    Tested conditioned response

--- app.js
+++ app.js
@@ -8,8 +8,7 @@ app.use('/webhook', middleware({
     channelAccessToken: process.env.LINE_ACCESS_TOKEN,
     channelSecret: process.env.LINE_CHANNEL_SECRET 
 }), (req, res) => {
-    const events = req.body.events;
-    Promise.all(events.map(eventHandler)).then(() => {
+    Promise.all(req.body.events.map(eventHandler)).then(() => {
         res.sendStatus(200);
     });
 });
