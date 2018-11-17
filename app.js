7f79782800b682e618dd12c80ee773de291bb04f
Satoshi Hirade <shirade222@gmail.com>
2018-11-17 19:47:08 +0900

    Added line lib

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const async = require('async');
-const { middleware } = require("@line/bot-sdk");
+const line = require('./lib/line');
 
 const app = express();
 const eventHandler = require('./lib/eventHandler.js')
@@ -9,10 +9,7 @@ app.use('/health', (req, res) => {
     res.sendStatus(200);
 });
 
-app.use('/webhook', middleware({
-    channelAccessToken: process.env.LINE_ACCESS_TOKEN,
-    channelSecret: process.env.LINE_CHANNEL_SECRET 
-}), (req, res) => {
+app.use('/webhook', line.middleware, (req, res) => {
     async.map(req.body.events, eventHandler, (err, events) => {
         err ? res.statusCode(500).send(err.toString()) : Promise.all(events).then(() => res.sendStatus(200))
     });
