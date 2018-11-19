b29e4b0532e7dc0a81485b287773ed9739061459
Satoshi Hirade <shirade222@gmail.com>
2018-11-19 23:25:27 +0900

    Simplified codes for Qiita

--- app.js
+++ app.js
@@ -1,19 +1,9 @@
 const express = require('express');
-const async = require('async');
-
-require('./lib/cron');
 const app = express();
-const line = require('./lib/line');
-const eventHandler = require('./lib/eventHandler.js');
+require('./lib/cron');
 
-app.use('/health', (req, res) => {
+app.get('/health', (_, res) => {
     res.sendStatus(200);
-});
-
-app.use('/webhook', line.middleware, (req, res) => {
-    async.map(req.body.events, eventHandler, (err, events) => {
-        err ? res.statusCode(500).send(err.toString()) : Promise.all(events).then(() => res.sendStatus(200))
-    });
 });
 
 module.exports = app;
